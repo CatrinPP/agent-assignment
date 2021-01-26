714cc918240b9343c3509fc3e911750c1cdbacf1
CatrinPP <caterina.parshina@gmail.com>
2021-01-27 02:49:38 +0300

    delete card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -31,12 +31,8 @@ const Card = ({
   deleteCard,
   deletePhoto,
 }) => {
-  if (!company || !contact) {
-    return <></>;
-  }
-
-  const phone = getPhoneFormated(contact.phone);
-  const companyType = getCompanyType(company.type);
+  const phone = contact ? getPhoneFormated(contact.phone) : null;
+  const companyType = company ? getCompanyType(company.type) : null;
 
   const renderPhoto = (photo) => (
     <li className="card__photo" key={photo.name}>
@@ -90,94 +86,102 @@ const Card = ({
           </li>
         </ul>
       </div>
-      <article className="card__content">
-        <h2 className="card__title">
-          <span>{company.shortName}</span>
-          <ButtonEdit onClick={() => {
-            showPopup('EditCompanyNamePopup', {
-              propsForBasePopup: {
-                actionBtnText: 'Сохранить',
-              },
-            });
-          }}
-          />
-        </h2>
-        <div className="card__section">
-          <p className="card__section-title card__section-title--edit">
-            Общая информация
-            <ButtonEdit onClick={() => {
-              showPopup('EditCompanyPopup', {
-                propsForBasePopup: {
-                  actionBtnText: 'Сохранить',
-                },
-              });
-            }}
-            />
-          </p>
-          <div className="card__section-info">
-            <div className="card__section-row">
-              <p className="card__section-key">Полное название</p>
-              <p className="card__section-value">{company.name}</p>
-            </div>
-            <div className="card__section-row">
-              <p className="card__section-key">Договор</p>
-              <p className="card__section-value">{company.contract.no} от <time dateTime={moment(company.contract.date).format('YYYY-MM-DD')}>{moment(company.contract.issue_date).format('DD.MM.YYYY')}</time></p>
-            </div>
-            <div className="card__section-row">
-              <p className="card__section-key">Форма</p>
-              <p className="card__section-value">{company.businessEntity}</p>
-            </div>
-            <div className="card__section-row">
-              <p className="card__section-key">Тип</p>
-              <p className="card__section-value  card__section-value--capitalized">{companyType}</p>
-            </div>
-          </div>
-        </div>
-        <div className="card__section">
-          <p className="card__section-title card__section-title--edit">
-            Контактные данные
-            <ButtonEdit onClick={() => {
-              showPopup('EditContactPopup', {
-                propsForBasePopup: {
-                  actionBtnText: 'Сохранить',
-                },
-              });
-            }}
-            />
-          </p>
-          <div className="card__section-info">
-            <div className="card__section-row">
-              <p className="card__section-key">ФИО</p>
-              <p className="card__section-value">{contact.lastname} {contact.firstname} {contact.patronymic}</p>
-            </div>
-            <div className="card__section-row">
-              <p className="card__section-key">Телефон</p>
-              <p className="card__section-value">{phone}</p>
-            </div>
-            <div className="card__section-row">
-              <p className="card__section-key">Эл.почта</p>
-              <p className="card__section-value card__section-value--colored"><a href="mailto:grigoriev@funeral.com">{contact.email}</a></p>
-            </div>
-          </div>
-        </div>
-        <div className="card__section">
-          <p className="card__section-title">Приложенные фото</p>
-          <ul className="card__photos">
-            {
-              company.photos.map((photo) => renderPhoto(photo))
-            }
-          </ul>
-          <button
-            className="button  button--add"
-            type="button"
-            onClick={() => {
-              showPopup('FileUploadPopup', {});
-            }}
-          >
-            <span>Добавить изображение</span>
-          </button>
-        </div>
-      </article>
+      {
+        company && contact
+          ? (
+            <article className="card__content">
+              <h2 className="card__title">
+                <span>{company.shortName}</span>
+                <ButtonEdit onClick={() => {
+                  showPopup('EditCompanyNamePopup', {
+                    propsForBasePopup: {
+                      actionBtnText: 'Сохранить',
+                    },
+                  });
+                }}
+                />
+              </h2>
+              <div className="card__section">
+                <p className="card__section-title card__section-title--edit">
+                  Общая информация
+                  <ButtonEdit onClick={() => {
+                    showPopup('EditCompanyPopup', {
+                      propsForBasePopup: {
+                        actionBtnText: 'Сохранить',
+                      },
+                    });
+                  }}
+                  />
+                </p>
+                <div className="card__section-info">
+                  <div className="card__section-row">
+                    <p className="card__section-key">Полное название</p>
+                    <p className="card__section-value">{company.name}</p>
+                  </div>
+                  <div className="card__section-row">
+                    <p className="card__section-key">Договор</p>
+                    <p className="card__section-value">{company.contract.no} от <time dateTime={moment(company.contract.date).format('YYYY-MM-DD')}>{moment(company.contract.issue_date).format('DD.MM.YYYY')}</time></p>
+                  </div>
+                  <div className="card__section-row">
+                    <p className="card__section-key">Форма</p>
+                    <p className="card__section-value">{company.businessEntity}</p>
+                  </div>
+                  <div className="card__section-row">
+                    <p className="card__section-key">Тип</p>
+                    <p className="card__section-value  card__section-value--capitalized">{companyType}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="card__section">
+                <p className="card__section-title card__section-title--edit">
+                  Контактные данные
+                  <ButtonEdit onClick={() => {
+                    showPopup('EditContactPopup', {
+                      propsForBasePopup: {
+                        actionBtnText: 'Сохранить',
+                      },
+                    });
+                  }}
+                  />
+                </p>
+                <div className="card__section-info">
+                  <div className="card__section-row">
+                    <p className="card__section-key">ФИО</p>
+                    <p className="card__section-value">{contact.lastname} {contact.firstname} {contact.patronymic}</p>
+                  </div>
+                  <div className="card__section-row">
+                    <p className="card__section-key">Телефон</p>
+                    <p className="card__section-value">{phone}</p>
+                  </div>
+                  <div className="card__section-row">
+                    <p className="card__section-key">Эл.почта</p>
+                    <p className="card__section-value card__section-value--colored"><a href="mailto:grigoriev@funeral.com">{contact.email}</a></p>
+                  </div>
+                </div>
+              </div>
+              <div className="card__section">
+                <p className="card__section-title">Приложенные фото</p>
+                <ul className="card__photos">
+                  {
+                    company.photos.map((photo) => renderPhoto(photo))
+                  }
+                </ul>
+                <button
+                  className="button  button--add"
+                  type="button"
+                  onClick={() => {
+                    showPopup('FileUploadPopup', {});
+                  }}
+                >
+                  <span>Добавить изображение</span>
+                </button>
+              </div>
+            </article>
+          ) : (
+            <p className="nodata">Данных нет</p>
+          )
+      }
+
     </div>
   );
 };
